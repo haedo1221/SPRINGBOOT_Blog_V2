54542b45fe474614245cce0ca9242bd2163ff540
haedo1221 <haedo1221@gmail.com>
2022-05-23 13:25:33 +0900

    11. 회원정보 수정 완료 - 더티체킹

--- src/main/java/site/metacoding/blogv2/web/api/UserApiController.java
+++ src/main/java/site/metacoding/blogv2/web/api/UserApiController.java
@@ -1,25 +1,42 @@
 package site.metacoding.blogv2.web.api;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.UserService;
 import site.metacoding.blogv2.web.api.dto.ResponseDto;
 import site.metacoding.blogv2.web.api.dto.user.JoinDto;
 import site.metacoding.blogv2.web.api.dto.user.LoginDto;
+import site.metacoding.blogv2.web.api.dto.user.UpdateDto;
 
 @RequiredArgsConstructor
 @RestController
 public class UserApiController {
     private final UserService userService;
     private final HttpSession session;
+
+    // password, email, addr
+    @PutMapping("/s/api/user/{id}")
+    public ResponseDto<?> update(@PathVariable Integer id, @RequestBody UpdateDto updateDto) {
+        userService.회원수정(id, updateDto);
+        return new ResponseDto<>(1, "성공", null);
+    }
+
+    // 우리 웹브라우저에서는 현재 사용안함. 추후 앱에서 요청시에 사용할 예정
+    @GetMapping("/s/api/user/{id}")
+    public ResponseDto<?> userInfo(@PathVariable Integer id) {
+        User userEntity = userService.회원정보(id);
+        return new ResponseDto<>(1, "성공", userEntity);
+    }
 
     // 회원가입 페이지주세요, 회원가입할꼐요, 로그인페이지주세요, 로그인할께요
     // 로그아웃할께요
@@ -28,35 +45,26 @@ public class UserApiController {
         session.invalidate();
         return new ResponseDto<>(1, "성공", null);
     }
-
     @PostMapping("/join")
     public ResponseDto<?> join(@RequestBody JoinDto joinDto) {
-
         userService.회원가입(joinDto);
-
         return new ResponseDto<>(1, "회원가입성공", null);
     }
-
     @PostMapping("/login")
     public ResponseDto<?> login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
         User userEntity = userService.로그인(loginDto);
-
         if (userEntity == null) {
             return new ResponseDto<>(-1, "로그인실패", null);
         }
-
         if (loginDto.getRemember().equals("on")) {
             response.addHeader("Set-Cookie", "remember=" + loginDto.getUsername() + "; path=/");
         }
-
         // response.addHeader("Set-Cookie", "remember=" + loginDto.getUsername() + ";
         // path=/; httpOnly=true");
         // response.addHeader("Set-Cookie", "path=/");
-
         // Cookie cookie = new Cookie("remember", loginDto.getUsername());
         // cookie.setPath("/");
         // response.addCookie(cookie);
-
         session.setAttribute("principal", userEntity);
         return new ResponseDto<String>(1, "로그인성공", null);
     }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -2,15 +2,43 @@
 $("#btn-join").click(() => {
     join();
 });
-
 $("#btn-login").click(() => {
     login();
 });
 
+$("#btn-update").click(() => {
+    update();
+});
 
 // 2. 기능
 
-// 유저네임 기억하기 메서드 httpOnly 속성이 걸려있으면 안된다 주의하자!!
+// 회원정보 수정 함수
+async function update() {
+    let id = $("#id").val();
+    let updateDto = {
+        password: $("#password").val(),
+        email: $("#email").val(),
+        addr: $("#addr").val()
+    }
+
+    let response = await fetch(`/s/api/user/${id}`, {
+        method: "PUT",
+        body: JSON.stringify(updateDto),
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        }
+    });
+    let responseParse = await response.json();
+
+    if (responseParse.code == 1) {
+        alert("업데이트 성공");
+        location.href = `/s/user/${id}`;
+    } else {
+        alert("업데이트 실패");
+    }
+}
+
+// 유저네임 기억하기 함수 httpOnly 속성이 걸려있으면 안된다 주의하자!!
 function usernameRemember() {
     let cookies = document.cookie.split("=");
     //console.log(cookies[1]);
@@ -18,48 +46,25 @@ function usernameRemember() {
 }
 usernameRemember();
 
-// 회원가입 요청 메서드
+// 회원가입 요청 함수
 async function join() {
     // (1) username, password, email, addr 을 찾아서 오브젝트로 만든다.
     let joinDto = {
-        username: $("#username").val(),
-        password: $("#password").val(),
-        email: $("#email").val(),
-        addr: $("#addr").val()
-    }
-
-    // (2) fetch 요청한다. (json으로 변환해서)
-    let response = await fetch("/join", {
-        method: "POST",
-        body: JSON.stringify(joinDto),
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-        },
-    });
-    let responseParse = await response.json();
-    console.log(responseParse);
-
-    // (3) 회원가입이 잘되면 알림창 띄우고 로그인 페이지로 이동한다.
-    if (responseParse.code == 1) {
-        alert("회원가입완료");
-        location.href = "/loginForm";
-    } else {
-        alert('회원가입실패');
+@@ -48,7 +77,7 @@ async function join() {
     }
 }
 
-// 로그인 요청 메서드
+
+// 로그인 요청 함수
 async function login() {
 
     // checkbox의 체크여부를 제이쿼리에서 확인하는 법
     let checked = $('#remember').is(':checked');
-
     let loginDto = {
         username: $("#username").val(),
         password: $("#password").val(),
         remember: checked ? "on" : "off"
     }
-
     let response = await fetch("/login", {
         method: "POST",
         body: JSON.stringify(loginDto),
@@ -69,7 +74,6 @@ async function login() {
     });
     let responseParse = await response.json();
     console.log(responseParse);
-
     if (responseParse.code == 1) {
         alert("로그인완료");
         location.href = "/";
