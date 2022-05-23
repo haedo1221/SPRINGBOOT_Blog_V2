af30864603e1954ec09aff552649ff93e30e5418
haedo1221 <haedo1221@gmail.com>
2022-05-23 14:03:05 +0900

    19. 댓글 리스트 보기 완료

--- src/main/java/site/metacoding/blogv2/web/PostController.java
+++ src/main/java/site/metacoding/blogv2/web/PostController.java
@@ -1,17 +1,13 @@
 package site.metacoding.blogv2.web;
-
 import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.domain.post.Post;
 import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.PostService;
-
 @RequiredArgsConstructor
 @Controller
 public class PostController {
@@ -20,10 +16,11 @@ public class PostController {
 
     @GetMapping("/post/{id}")
     public String detail(@PathVariable Integer id, Model model) {
+        Post postEntity = postService.글상세보기(id);
+        model.addAttribute("comments", postEntity.getComments());
         model.addAttribute("postId", id);
         return "post/detail";
     }
-
     // 페이지를 줘
     // /s 붙었으니까 자동으로 인터셉터가 인증 체크함. (완)
     @GetMapping("/s/post/writeForm")
