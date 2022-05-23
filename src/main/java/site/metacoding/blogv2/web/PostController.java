92fcc5a2a09df9026f59829b3aa763608f6f0300
haedo1221 <haedo1221@gmail.com>
2022-05-23 13:47:48 +0900

    15. 글 목록 보기 + 페이징까지 완료

--- src/main/java/site/metacoding/blogv2/web/PostController.java
+++ src/main/java/site/metacoding/blogv2/web/PostController.java
@@ -1,8 +1,12 @@
 package site.metacoding.blogv2.web;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.service.PostService;
+
 @RequiredArgsConstructor
 @Controller
 public class PostController {
--- src/main/java/site/metacoding/blogv2/web/api/PostApiController.java
+++ src/main/java/site/metacoding/blogv2/web/api/PostApiController.java
@@ -1,23 +1,32 @@
 package site.metacoding.blogv2.web.api;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.domain.post.Post;
 import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.PostService;
 import site.metacoding.blogv2.web.api.dto.ResponseDto;
 import site.metacoding.blogv2.web.api.dto.post.WriteDto;
-
 @RequiredArgsConstructor
 @RestController
 public class PostApiController {
     private final PostService postService;
     private final HttpSession session;
+
+    @GetMapping("/api/post")
+    public ResponseDto<?> list(Integer page) {
+        Page<Post> posts = postService.게시글목록(page);
+        // 응답의 DTO를 만들어서 <- posts 를 옮김. (라이브러리 있음)
+        return new ResponseDto<>(1, "성공", posts);
+    }
 
     @PostMapping("/s/post")
     public ResponseDto<?> write(@RequestBody WriteDto writeDto) {
@@ -26,7 +35,6 @@ public class PostApiController {
         Post post = writeDto.toEntity(principal);
         // 원래는 그냥 dto바로 넘겼는데, 지금 dto를 넘기면 session값 두개 넘겨야 해서 하나로 합쳐서 넘김
         postService.글쓰기(post);
-
         return new ResponseDto<>(1, "성공", null);
     }
 }
