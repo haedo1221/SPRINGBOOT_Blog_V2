794b70f42674b86d6a4e2988ad279226d0eae9d3
haedo1221 <haedo1221@gmail.com>
2022-05-23 13:49:46 +0900

    16. 글 상세보기 완료

--- src/main/java/site/metacoding/blogv2/service/PostService.java
+++ src/main/java/site/metacoding/blogv2/service/PostService.java
@@ -1,12 +1,13 @@
 package site.metacoding.blogv2.service;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.domain.post.Post;
 import site.metacoding.blogv2.domain.post.PostRepository;
@@ -16,11 +17,20 @@ import site.metacoding.blogv2.domain.post.PostRepository;
 public class PostService {
     private final PostRepository postRepository;
 
+    public Post 글상세보기(Integer id) {
+        Optional<Post> postOp = postRepository.findById(id);
+
+        if (postOp.isPresent()) {
+            return postOp.get();
+        } else {
+            throw new RuntimeException("해당 게시글을 찾을 수 없습니다");
+        }
+    }
+
     public Page<Post> 게시글목록(Integer page) {
         PageRequest pq = PageRequest.of(page, 3, Sort.by(Direction.DESC, "id"));
         return postRepository.findAll(pq);
     }
-
     @Transactional
     public void 글쓰기(Post post) {
         postRepository.save(post);
