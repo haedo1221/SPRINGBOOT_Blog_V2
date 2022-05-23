56fcda4ace696614619d04aa046ff995fa766b3d
haedo1221 <haedo1221@gmail.com>
2022-05-23 13:13:48 +0900

    9. 인증 프로세스 - 네비게이션 변경, 로그아웃 구현 완료

--- src/main/java/site/metacoding/blogv2/web/UserController.java
+++ src/main/java/site/metacoding/blogv2/web/UserController.java
@@ -1,17 +1,23 @@
 package site.metacoding.blogv2.web;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv2.service.UserService;
-
 @RequiredArgsConstructor
 @Controller
 public class UserController {
     private final UserService userService;
+    private final HttpSession session;
+
+    @GetMapping("/logout")
+    public String logout() {
+        session.invalidate(); // 세션 무효화 (세션 아이디 영역의 데이터를 다 삭제해)
+        return "redirect:/";
+    }
 
     // 웹브라우저 -> 회원가입 페이지 주세요!!
     // 앱 -> 회원가입 페이지 주세요!! 말이 안됨!!
@@ -19,7 +25,6 @@ public class UserController {
     public String joinForm() {
         return "user/joinForm";
     }
-
     @GetMapping("/loginForm")
     public String loginForm() {
         return "user/loginForm";
