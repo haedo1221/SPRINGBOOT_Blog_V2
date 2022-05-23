77d4a6adda51d864b6a629d8387f0500c274d139
haedo1221 <haedo1221@gmail.com>
2022-05-23 13:28:19 +0900

    [회원수정 세션값 변경 처리]

--- src/main/java/site/metacoding/blogv2/handler/ErrorHandler.java
+++ src/main/java/site/metacoding/blogv2/handler/ErrorHandler.java
@@ -1,7 +1,6 @@
 package site.metacoding.blogv2.handler;
 
-import org.hibernate.exception.SQLGrammarException;
-import org.springframework.dao.DataIntegrityViolationException;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
