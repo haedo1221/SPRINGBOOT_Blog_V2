2000c877f1480ec8f8b004befb5bd0475909e3d3
haedo1221 <haedo1221@gmail.com>
2022-05-23 13:53:27 +0900

    [글 상세보기 권한 처리 완료]

--- src/main/java/site/metacoding/blogv2/domain/post/Post.java
+++ src/main/java/site/metacoding/blogv2/domain/post/Post.java
@@ -1,8 +1,6 @@
 package site.metacoding.blogv2.domain.post;
-
 import java.time.LocalDateTime;
 import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
@@ -15,24 +13,22 @@ import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import site.metacoding.blogv2.domain.comment.Comment;
 import site.metacoding.blogv2.domain.user.User;
-
 /**
  * GET /post/1 상세보기
  * User, Post, List<Comment>
  */
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -47,20 +43,17 @@ public class Post { // N (드라이빙 테이블, FK의 주인)
     @Lob
     @Column(nullable = false)
     private String content;
-
-     // @ColumnDefault("0") 쓰지마세요!!
+    // @ColumnDefault("0") 쓰지마세요!!
     @Column(nullable = false)
     private Integer pageCount; // 조회수
 
+    @JsonIgnoreProperties({ "password" })
     @JoinColumn(name = "userId")
     @ManyToOne(fetch = FetchType.EAGER)
     private User user;
-
-    
     @JsonIgnoreProperties({ "post" }) // messageConverter에게 알려주는 어노테이션
     @OneToMany(mappedBy = "post") // 연관관계의 주인의 변수명
     private List<Comment> comments;
-
     @CreatedDate // insert
     private LocalDateTime createDate;
     @LastModifiedDate // insert, update
--- src/main/java/site/metacoding/blogv2/web/api/PostApiController.java
+++ src/main/java/site/metacoding/blogv2/web/api/PostApiController.java
@@ -1,11 +1,12 @@
 package site.metacoding.blogv2.web.api;
-
 import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,31 @@ import site.metacoding.blogv2.domain.post.Post;
 import site.metacoding.blogv2.domain.user.User;
 import site.metacoding.blogv2.service.PostService;
 import site.metacoding.blogv2.web.api.dto.ResponseDto;
+import site.metacoding.blogv2.web.api.dto.post.DetailResponseDto;
 import site.metacoding.blogv2.web.api.dto.post.WriteDto;
+
 @RequiredArgsConstructor
 @RestController
 public class PostApiController {
     private final PostService postService;
     private final HttpSession session;
+
+    @GetMapping("/api/post/{id}")
+    public ResponseDto<?> detail(@PathVariable Integer id) {
+        Post postEntity = postService.글상세보기(id);
+        User principal = (User) session.getAttribute("principal");
+        boolean auth = false;
+
+        if (principal != null) {
+
+            if (principal.getId() == postEntity.getUser().getId()) {
+                auth = true;
+            }
+        }
+
+        DetailResponseDto detailResponseDto = new DetailResponseDto(postEntity, auth);
+        return new ResponseDto<>(1, "성공", detailResponseDto);
+    }
 
     @GetMapping("/api/post")
     public ResponseDto<?> list(Integer page) {
@@ -27,10 +47,8 @@ public class PostApiController {
         // 응답의 DTO를 만들어서 <- posts 를 옮김. (라이브러리 있음)
         return new ResponseDto<>(1, "성공", posts);
     }
-
     @PostMapping("/s/post")
     public ResponseDto<?> write(@RequestBody WriteDto writeDto) {
-
         User principal = (User) session.getAttribute("principal");
         Post post = writeDto.toEntity(principal);
         // 원래는 그냥 dto바로 넘겼는데, 지금 dto를 넘기면 session값 두개 넘겨야 해서 하나로 합쳐서 넘김
